817cabf617780837b5c38de75f4072585950fa9e
Treasure Kabareebe <treasurecalm@gmail.com>
2020-03-03 19:11:27 +0300

    Add simple show page to display signed up user

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -4,19 +4,21 @@ class UserController < ApplicationController
       end
     
     def create
-    @user = User.new(user_params)
-    if @user.save
-        sign_in @user
-        flash[:success] = 'Welcome to Members Only!'
-        # redirect_to posts_path
-    else
-        render 'new'
+        @user = User.new(user_params)
+        if @user.save
+            redirect_to user_path(@user)
+        else
+            render 'new'
+        end
     end
+
+    def show
+        @users = User.all
     end
 
     private
 
-    def user_params
-    params.require(:user).permit(:username, :email, :password_digest, :password_confirmation)
-    end
+        def user_params
+            params.require(:user).permit(:username, :email, :password, :password_confirmation)
+        end
 end
