e48d9fab6c861f3e7aef1214b2f3ed345dadf8e4
Treasure Kabareebe <treasurecalm@gmail.com>
2020-03-04 00:55:53 +0300

    Create an EventsController
    Add an EventsController and corresponding routes to allow you to create an event, show a single event, and list all events.

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,29 @@
+class EventsController < ApplicationController
+  def new
+    @event = Event.new
+  end
+
+  def index
+    @events = Event.all
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def create
+    @event = current_user.event.build(event_params)
+    if @event.save
+        redirect_to events_index_path
+    else
+        render 'new'
+    end
+  end
+
+  private
+
+    def event_params
+        params.require(:event).permit(:name, :date, :description, :venue)
+    end
+
+end
