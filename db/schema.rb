dff8d271fbbb4d9c8b82959e9cbaa1c84a16a363
Amadou Ibrahim <amadou140589@gmail.com>
2020-03-03 18:40:23 +0100

    Event Model Build and migrate.Association for event and user created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_02_203258) do
+ActiveRecord::Schema.define(version: 2020_03_03_172123) do
+
+  create_table "events", force: :cascade do |t|
+    t.string "name"
+    t.datetime "date"
+    t.string "description"
+    t.string "venue"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_events_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
