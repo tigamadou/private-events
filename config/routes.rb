04151ed683508a68000542d3ebfc48f537233f62
Amadou Ibrahim <amadou140589@gmail.com>
2020-03-04 22:15:03 +0100

    Navigation links added to the navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'events/new'
-  get 'events/index'
+  get 'events', to: 'events#index'
   get 'events/:id', to: 'events#show'
   post 'events/new', to: 'events#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
