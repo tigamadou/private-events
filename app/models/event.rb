7d246f5369eedfe2957a696cc6206e05b3131c69
Treasure Kabareebe <treasurecalm@gmail.com>
2020-03-04 23:50:32 +0300

    Refactor the “upcoming” and “past” methods into simple scopes

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,13 +3,16 @@ class Event < ApplicationRecord
     has_many :event_attendees
     has_many :attendees, class_name: "User", through: :event_attendees
 
-    def self.past
-        Event.all.where("date < ?", Time.now)
-    end
+    # def self.past
+    #     Event.all.where("date < ?", Time.now)
+    # end
 
-    def self.upcoming
-        Event.all.where("date > ?", Time.now)
-    end
+    # def self.upcoming
+    #     Event.all.where("date > ?", Time.now)
+    # end
+
+    scope :upcoming, -> { where("date > ?", Time.now) }
+    scope :past, -> { where("date < ?", Time.now) }
 
     scope :upcoming_events, -> { where("date > ?", Time.now) }
     scope :previous_events, -> { where("date < ?", Time.now) }
