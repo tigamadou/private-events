15b8caf31bd63182c2ca043a5591fb1608edb320
Treasure Kabareebe <treasurecalm@gmail.com>
2020-03-04 23:46:41 +0300

    Modify the Event Index page to list all events, separated into Past and Upcoming categories.

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,6 +3,14 @@ class Event < ApplicationRecord
     has_many :event_attendees
     has_many :attendees, class_name: "User", through: :event_attendees
 
+    def self.past
+        Event.all.where("date < ?", Time.now)
+    end
+
+    def self.upcoming
+        Event.all.where("date > ?", Time.now)
+    end
+
     scope :upcoming_events, -> { where("date > ?", Time.now) }
     scope :previous_events, -> { where("date < ?", Time.now) }
 end
