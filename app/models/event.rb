60cfebf642c684a8caf77f2642cdbec320f071d1
Amadou Ibrahim <amadou140589@gmail.com>
2020-03-06 21:16:15 +0100

    Event Model Tests

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,7 @@ class Event < ApplicationRecord
     belongs_to :creator, class_name: "User", foreign_key: :user_id
     has_many :event_attendees
     has_many :attendees, class_name: "User", through: :event_attendees
-
+    validates :name, :description, :date, :venue, presence: true
     # def self.past
     #     Event.all.where("date < ?", Time.now)
     # end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,52 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:event) { Event.new(name: 'Birthdat Party', description: 'Simple Description', date: '2020-03-06',venue:'White House')}
+
+  context 'it has name, description, date and venue' do
+    it 'has to have a name' do
+      expect(event.name).to eq('Birthdat Party')
+    end
+  
+    it 'it has to have a description' do
+      expect(event.description).to eq('Simple Description')
+    end
+  
+    it 'it has to have a date' do
+      expect(event.date).to eq('2020-03-06')
+    end
+
+    it 'it has to have a venue' do
+      expect(event.venue).to eq('White House')
+    end
+
+  end
+  
+  context 'it has to return an error for missing fields' do
+    it 'returns an error for missing name' do
+      event.name = nil
+      expect(event.save).to eq(false)
+    end
+
+    it 'returns an error for missing description' do
+      event.description = nil
+      expect(event.save).to eq(false)
+    end
+
+    it 'returns an error for missing date' do
+      event.date = nil
+      expect(event.save).to eq(false)
+    end
+
+    it 'returns an error for missing venue' do
+      event.venue = nil
+      expect(event.save).to eq(false)
+    end
+
+  end
+
+  context 'It has Associations' do
+    it { should belong_to(:creator) }
+    it { should have_many(:event_attendees) }
+  end
 end
