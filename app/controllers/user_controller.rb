8eae302dba779846f2ade7e4bca78324a064fcd9
Amadou Ibrahim <amadou140589@gmail.com>
2020-03-12 17:41:39 +0100

    User controller layout for new action edited

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,10 +1,12 @@
 class UserController < ApplicationController
+
   def new
+    layout 'home'
     @user = User.new
   end
 
   def create
-    layout 'home'
+    
     @user = User.new(user_params)
     if @user.save
       sign_in @user
