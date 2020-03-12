835bb28b1a97db7cd1eb67d4f652f41334e6cdb2
Amadou Ibrahim <amadou140589@gmail.com>
2020-03-12 17:47:29 +0100

    Rubocop errors fixed

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,12 +1,10 @@
 class UserController < ApplicationController
-
   def new
     layout 'home'
     @user = User.new
   end
 
   def create
-    
     @user = User.new(user_params)
     if @user.save
       sign_in @user
